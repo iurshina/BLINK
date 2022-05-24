c8b930146b816a9ef450513b5cc60a655db609db
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-05-24 13:21:25 +0200

    Script to convert mentions to BLINK format

---
+++ data_scripts/mentions2blink.py
@@ -0,0 +1,37 @@
+# {
+#   "context_left": "3 console.\nSome users claim that Nvidia's Linux drivers impose artificial restrictions, like limiting the number of monitors that can be used at the same time, but the company has not commented on these accusations.\nIn 2014, with Maxwell GPUs, Nvidia started to require firmware by them to unlock all features of its graphics cards. Up to now, this state has not changed and makes writing open-source drivers difficult.\nDeep learning.\nNvidia GPUs are often used in deep learning, and accelerated analytics due to Nvidia's API CUDA which allows programmers to utilize the higher number of cores present in GPUs to parallelize programs. This parallelization can dramatically increase training speed of machine learning algorithms due to their extensive use of matrix and vector operations. They were included in many Tesla vehicles before",
+#   "context_right": "announced at Tesla Autonomy Day in 2019 that the company developed its own SoC and Full Self-Driving computer now and would stop using Nvidia hardware for their vehicles. According to \"TechRepublic\", Nvidia GPUs \"work well for deep learning tasks because they are designed for parallel computing and do well to handle the vector and matrix operations that are prevalent in deep learning\". These GPUs are used by researchers, laboratories, tech companies and enterprise companies. In 2009, Nvidia was involved in what was called the \"big bang\" of deep learning, \"as deep-learning neural networks were combined with Nvidia graphics processing units (GPUs)\". That year, the Google Brain used Nvidia GPUs to create Deep Neural Networks capable of machine learning, where Andrew Ng determined that GPUs could increase the speed",
+#   "mention": "Elon Musk",
+#   "label_title": "Elon Musk",
+#   "label": "Elon Reeve Musk (; born June 28, 1971) is an entrepreneur and business magnate. He is the founder, CEO and chief engineer at SpaceX; early stage investor, CEO, and product architect of Tesla, Inc.; founder of The Boring Company; and co-founder of Neuralink and OpenAI. A centibillionaire, Musk is one of the richest people in the world.\nMusk was born to a Canadian mother and South African father and raised in Pretoria, South Africa. He briefly attended the University of Pretoria before moving to Canada aged 17 to attend Queen's University. He transferred to the University of Pennsylvania two years later, where he received bachelors' degrees in economics and physics. He moved to California in 1995 to attend Stanford University but decided instead to pursue a business career, co-founding",
+#   "label_id": 304371
+# }
+#
+
+label_map = {}
+
+with open("documents.json") as f:
+    for l in f:
+        line = json.loads(l)
+        id = line["document_id"]
+        title = line["title"]
+        text = line["text"]
+
+        label_map[id] = (title, text)
+
+
+with open("/workspace/nilk_data/test.json") as f, open("test.json", "w") as o, open("test_nil.json", "w") as on:
+    for l in f:
+        line = json.loads(l)
+
+        id = line["wikidata_id"]
+        is_nil = line["nil"]
+        mention = line["mention"]
+        context = line["context"]
+        offset = line["offset"]
+        if not is_nil:
+            title, text = label_map[id]
+
+            o.write(json.dumps({"context_left": context[:offset], "context_right": context[offset + len(mention):], "mention": mention, "label_title": title, "label": text, "label_id": id}) + "\n")
+        else:
+            on.write(json.dumps({"context_left": context[:offset], "context_right": context[offset + len(mention):], "mention": mention}) + "\n")
--- data_scripts/wikidata2blink.py
+++ data_scripts/wikidata2blink.py
@@ -1,5 +1,10 @@
 import json, gzip
 
+# {
+#   "title": "Steve Jobs",
+#   "text": "Steven Paul Jobs (; February 24, 1955 – October 5, 2011) was an American business magnate, industrial designer, investor, and media proprietor. He was the chairman, chief executive officer (CEO), and co-founder of Apple Inc.; the chairman and majority shareholder of Pixar; a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and the founder, chairman, and CEO of NeXT. Jobs is widely recognized as a pioneer of the personal computer revolution of the 1970s and 1980s, along with his early business partner and fellow Apple co-founder Steve Wozniak.\nJobs was born in San Francisco, California, and put up for adoption. He was raised in the San Francisco Bay Area. He attended Reed College in 1972 before dropping out that same year, and traveled",
+#   "document_id": 7412236
+# }
 
 with gzip.open("/workspace/nilk_data/wikidata-20170213-all.json.gz", 'rb', 'rb') as gf, open("documents.json", "w") as o:
     for ln in gf:
