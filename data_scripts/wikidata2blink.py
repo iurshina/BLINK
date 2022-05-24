c071cc8809dd10bf33ff51d4ca55d0bd0c8081ce
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-05-24 13:06:40 +0200

    Script to convert wikidata to BLINK format

---
+++ data_scripts/wikidata2blink.py
@@ -0,0 +1,23 @@
+import json, gzip
+
+
+with gzip.open("/workspace/nilk_data/wikidata-20170213-all.json.gz", 'rb', 'rb') as gf, open("documents.json", "w") as o:
+    for ln in gf:
+        if ln == b'[\n' or ln == b']\n':
+            continue
+        if ln.endswith(b',\n'):
+            obj = json.loads(ln[:-2])
+        else:
+            obj = json.loads(ln)
+
+        id = obj["id"]
+        name = obj["labels"]["en"]["value"]
+
+        description = obj["descriptions"]["en"]["value"]
+
+        o.write(json.dumps({"document_id": id, "title": name, "text": description}) + "\n")
+
+
+
+
+
