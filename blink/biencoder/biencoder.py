d83ae2b35fce58b628a8b9e179e607e698678b5b
Anastasiia Iurshina <Anastasiia.Iurshina@ipvs.uni-stuttgart.de>
2022-05-25 12:29:01 +0200

    move to transformers

--- blink/biencoder/biencoder.py
+++ blink/biencoder/biencoder.py
@@ -57,12 +57,12 @@ class BiEncoderModule(torch.nn.Module):
         embedding_ctxt = None
         if token_idx_ctxt is not None:
             embedding_ctxt = self.context_encoder(
-                token_idx_ctxt, segment_idx_ctxt, mask_ctxt
+                token_idx_ctxt #, segment_idx_ctxt, mask_ctxt
             )
         embedding_cands = None
         if token_idx_cands is not None:
             embedding_cands = self.cand_encoder(
-                token_idx_cands, segment_idx_cands, mask_cands
+                token_idx_cands #, segment_idx_cands, mask_cands
             )
         return embedding_ctxt, embedding_cands
 
